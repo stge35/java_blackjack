fe973b4d15d5478310207538ca94630494239281
stge35 <evanok1@gmail.com>
2023-12-18 16:59:31 +0900

    Init

---
+++ src/main/java/blackjack/app/JavaBlackjackApplication.java
@@ -0,0 +1,13 @@
+package blackjack.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlackjackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBlackjackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blackjack/app/JavaBlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package blackjack.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlackjackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
